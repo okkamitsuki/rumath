c26024e240f373d20012beea3af0f4c3b54af1be
okkamitsuki <okkamitsuki@gmail.com>
2025-11-12 21:17:30 -0600

    add rum_orthographic_vulkan in projection.h

--- include/rumath/projection.h
+++ include/rumath/projection.h
@@ -9,6 +9,7 @@ extern "C" {
     rum_matrix4x4 rum_perspective_opengl(float fov_degrees, float aspect_ratio, float near_plane, float far_plane);
     rum_matrix4x4 rum_orthographic_opengl(float left, float right,float bottom, float top, float near_plane, float far_plane);
     rum_matrix4x4 rum_perspective_vulkan(float fov_degrees, float aspect_ratio, float near_plane, float far_plane);
+    rum_matrix4x4 rum_orthographic_vulkan(float left, float right, float bottom, float top, float near_plane, float far_plane);
 
 #ifdef  __cplusplus
 }
