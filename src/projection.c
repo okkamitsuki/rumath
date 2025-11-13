9e6800c01b5ad2bf4439fc2c174aa73d7291709e
okkamitsuki <okkamitsuki@gmail.com>
2025-11-12 21:16:55 -0600

    add rum_orthographic_vulkan

--- src/projection.c
+++ src/projection.c
@@ -46,3 +46,17 @@ rum_matrix4x4 rum_perspective_vulkan(float fov_radians, float aspect_ratio, floa
 
     return result;
 }
+
+rum_matrix4x4 rum_orthographic_vulkan(float left, float right, float bottom, float top, float near_plane, float far_plane) {
+    rum_matrix4x4 result = rum_matrix4x4_identity();
+
+    result.data[0][0] = 2.0f / (right - left);
+    result.data[1][1] = 2.0f / (top - bottom);
+    result.data[2][2] = 1.0f / (far_plane - near_plane);
+
+    result.data[3][0] = -(right + left) / (right - left);
+    result.data[3][1] = -(top + bottom) / (top - bottom);
+    result.data[3][2] = -near_plane / (far_plane - near_plane);
+
+    return result;
+}
